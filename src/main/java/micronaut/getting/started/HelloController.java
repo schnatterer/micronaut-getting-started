c93b115f6a922c50679b0396f194bb216cc70dfa
schnatterer <johannes@schnatterer.info>
2019-05-01 13:46:41 +0200

    Adds a simple Hello Controller.
    In order to get a response from the running app via HTTP.

---
+++ src/main/java/micronaut/getting/started/HelloController.java
@@ -0,0 +1,14 @@
+package micronaut.getting.started;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+
+@Controller("/")
+public class HelloController {
+
+    @Get(produces = MediaType.TEXT_PLAIN)
+    public String index() {
+        return "Hello World";
+    }
+}
